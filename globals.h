3521862b5fa3766db7b836ca2e739228e13695e5
StefanTcv <67293627+StefanTcv@users.noreply.github.com>
2020-06-23 00:34:23 +0300

    Create globals.h

---
+++ globals.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <fstream>
+#include <string>
+
+extern std::string file_name;
+
+extern std::ofstream output_to_file;
+extern std::ifstream input_from_file;
+
+extern bool should_save_to_file;
+extern bool should_read_from_file;
+
